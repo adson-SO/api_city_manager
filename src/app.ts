d18ba9b9467c9f3b1a964d3a1d5380617839359c
Adson Sousa de Oliveira <adson.oliveira@compasso.com.br>
2022-04-11 15:12:02 -0300

    chore: create app and server

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+export class App {
+    public server: any;
+
+    constructor() {
+        this.server = express();
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares(): void {
+        this.server.use(express.json());
+    }
+
+    routes(): void {
+        
+    }
+}

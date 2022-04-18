7b259836eab5b6b0a6c83a31001490a9f2093f5a
Adson Sousa de Oliveira <adson.oliveira@compasso.com.br>
2022-04-18 12:41:47 -0300

    test: create test to validate information sent in the body

--- __tests__/integration/client.test.ts
+++ __tests__/integration/client.test.ts
@@ -27,5 +27,66 @@ describe("client's routes", () => {
         });
 
         expect(result.statusCode).toBe(201);
-    })
+    });
+
+    it("should not create a new client without information sent in the body", async () => {
+        const result = await request(app).post("/api/v1/client").send({});
+
+        expect(result.statusCode).toBe(400);
+    });
+
+    it("should not create a new client without field city_id", async () => {
+        const result = await request(app).post("/api/v1/client").send({
+            fullname: "Name Test",
+            gender: "m",
+            birthdate: "12/04/2002",
+            age: 20
+        });
+
+        expect(result.statusCode).toBe(400);
+    });
+
+    it("should not create a new client without field age", async () => {
+        const result = await request(app).post("/api/v1/client").send({
+            fullname: "Name Test",
+            gender: "m",
+            birthdate: "12/04/2002",
+            city_id: "e5880de8-aaac-4b40-8651-3d4d422eb2ec"
+        });
+
+        expect(result.statusCode).toBe(400);
+    });
+
+    it("should not create a new client without field birthdate", async () => {
+        const result = await request(app).post("/api/v1/client").send({
+            fullname: "Name Test",
+            gender: "m",
+            age: 20,
+            city_id: "e5880de8-aaac-4b40-8651-3d4d422eb2ec"
+        });
+
+        expect(result.statusCode).toBe(400);
+    });
+
+    it("should not create a new client without field gender", async () => {
+        const result = await request(app).post("/api/v1/client").send({
+            fullname: "Name Test",
+            birthdate: "12/04/2002",
+            age: 20,
+            city_id: "e5880de8-aaac-4b40-8651-3d4d422eb2ec"
+        });
+
+        expect(result.statusCode).toBe(400);
+    });
+
+    it("should not create a new client without field fullname", async () => {
+        const result = await request(app).post("/api/v1/client").send({
+            gender: "m",
+            birthdate: "12/04/2002",
+            age: 20,
+            city_id: "e5880de8-aaac-4b40-8651-3d4d422eb2ec"
+        });
+
+        expect(result.statusCode).toBe(400);
+    });
 });
